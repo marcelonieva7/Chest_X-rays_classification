aa20ab4bc554ca99371c6704c2665587d6cb0a32
Marcelo Nieva <marcelonieva7@gmail.com>
2023-12-17 18:26:41 -0300

    add docstring for docs

--- predict.py
+++ predict.py
@@ -1,13 +1,60 @@
 from flask import Flask
-from flask import request, jsonify
+from flask import request, jsonify, render_template
+from flasgger import Swagger
 
 from server.model import interpreter, inp_index, out_index
 from server.controllers import get_prediction
 
 app = Flask('x-ray-classifier')
+swagger = Swagger(app)
+
+@app.route("/")
+def home():
+  return render_template("index.html")
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
+  """
+  Endpoint for making predictions based on an input URL.
+
+  ---
+  tags:
+    - Prediction
+  parameters:
+    - name: url
+      in: body
+      required: true
+      schema:
+          type: object
+          properties:
+            url:
+              type: string
+              description: URL of the image to be classified.
+              example: "https://raw.githubusercontent.com/marcelonieva7/Chest_X-rays_classification/main/test/data/normal.jpeg"
+
+  responses:
+    200:
+      description: Prediction results in JSON format.
+      schema:
+        type: object
+        properties:
+          Covid:
+            type: number
+            description: Probability of the image belonging to the Covid class.
+          Normal:
+            type: number
+            description: Probability of the image belonging to the Normal class.
+          Viral Pneumonia:
+            type: number
+            description: Probability of the image belonging to the Viral Pneumonia class.
+      examples:
+        {
+          "Covid": 1.671286940574646,
+          "Normal": 3.7960562705993652,
+          "Viral Pneumonia": 1.1494133472442627
+        }
+  """
   prediction = get_prediction(request.json['url'], interpreter, inp_index, out_index)
   return jsonify(prediction)
 
